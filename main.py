b139b243dc28d80a0dd51725f1a783fc35cad80e
Nicholas H.Tollervey <ntoll@ntoll.org>
2024-05-09 14:16:44 +0100

    Fixed character bleeding.

--- main.py
+++ main.py
@@ -21,8 +21,8 @@ async def type_it_in(code):
             wait = random.choice([0.05, 0.07, 0.1, 0.15, 0.2, 0.3])
             await asyncio.sleep(wait)
             python_terminal.terminal.write(char)
-        python_terminal.process(line)
+        python_terminal.process(line.strip())
 
 
 # Web scale use of advanced AI.
-await type_it_in('print("Hello, from PyScript!")\n# Your turn...')
+await type_it_in('print("Hello, from PyScript!")\r\n# Your turn...')
