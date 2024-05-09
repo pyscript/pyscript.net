b654cb7778b525ed4eb1919fdb95c90ac32cdf73
Nicholas H.Tollervey <ntoll@ntoll.org>
2024-05-09 16:00:56 +0100

    Minoy Pythonic update.

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import random
 from pyscript import document
 
 
-# It's the same API as JavaScript when interacting with the document object!
+# It's the same API as JS when using the document object!
 python_terminal = document.getElementById("python-terminal")
 
 
@@ -18,11 +18,16 @@ async def type_it_in(code):
     for line in lines:
         await asyncio.sleep(1)
         for char in line:
-            wait = random.choice([0.05, 0.07, 0.1, 0.15, 0.2, 0.3])
+            wait = random.choice(
+                [0.05, 0.07, 0.1, 0.15, 0.2, 0.3]
+            )
             await asyncio.sleep(wait)
             python_terminal.terminal.write(char)
         python_terminal.process(line.strip())
 
 
 # Web scale use of advanced AI.
-await type_it_in('print("Hello, from PyScript!")\r\n# Your turn...')
+await type_it_in(
+    'print("Hello, from PyScript!")\r\n'
+    '# Your turn...'
+)
